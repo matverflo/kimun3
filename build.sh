5836211aecd6fb383bac7f4cee69490d17fa5c88
matverflo <matvergaraf@udd.cl>
2026-08-12 14:44:40 -0400

    Auto-seed: Agregar script para cargar Curso de Adulto Mayor en producción

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 pip install -r requirements-deploy.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "exec(open('seed_curso_adulto_mayor.py', encoding='utf-8').read())"
