a35b48eedf218504f2c2e1944354970dca11548f
matverflo <matvergaraf@udd.cl>
2026-08-15 13:46:20 -0400

    chore: Remove seed files from Git tracking and build script

--- build.sh
+++ build.sh
@@ -2,7 +2,3 @@
 pip install -r requirements-deploy.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py seed_empleados
-python manage.py seed_pacientes
-python manage.py seed_cursos
-python manage.py seed_inscripciones
