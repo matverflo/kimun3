ba0083f4898f24eac987ec0acee6d62926fe4891
matverflo <matvergaraf@udd.cl>
2026-08-15 13:39:59 -0400

    chore: Add database seed commands to Vercel build script

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@
 pip install -r requirements-deploy.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py seed_empleados
+python manage.py seed_pacientes
+python manage.py seed_cursos
+python manage.py seed_inscripciones
